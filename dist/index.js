cf49e630cbed42a479511331c011011c09924aad
mayank2808sharma <mayank2808sharma@gmail.com>
2024-02-12 01:26:59 +0530

    completing the upload service

--- dist/index.js
+++ dist/index.js
@@ -20,27 +20,26 @@ const file_1 = require("./file");
 const path_1 = __importDefault(require("path"));
 const aws_1 = require("./aws");
 const redis_1 = require("redis");
-const publisher = (0, redis_1.createClient)();
+const publisher = (0, redis_1.createClient)(); // to store id in the queue
 publisher.connect();
-const subscriber = (0, redis_1.createClient)();
+const subscriber = (0, redis_1.createClient)(); // to store about the status being uploaded or deployed
 subscriber.connect();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.post("/deploy", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const repoUrl = req.body.repoUrl;
-    const id = (0, utils_1.generate)(); // asd12
-    yield (0, simple_git_1.default)().clone(repoUrl, path_1.default.join(__dirname, `output/${id}`));
+    const id = (0, utils_1.generate)(); // something like asd12
+    yield (0, simple_git_1.default)().clone(repoUrl, path_1.default.join(__dirname, `output/${id}`)); // clones the repo in dist/output
     const files = (0, file_1.getAllFiles)(path_1.default.join(__dirname, `output/${id}`));
     files.forEach((file) => __awaiter(void 0, void 0, void 0, function* () {
         const filename = path_1.default.relative(__dirname, file).replace(/\\/g, "/");
-        yield (0, aws_1.uploadFile)(filename, file);
+        yield (0, aws_1.uploadFile)(filename, file); // upload all the files to the R2 bucket
     }));
-    yield new Promise((resolve) => setTimeout(resolve, 5000));
-    publisher.lPush("build-queue", id);
-    // INSERT => SQL
-    // .create =>
-    publisher.hSet("status", id, "uploaded");
+    yield new Promise((resolve) => setTimeout(resolve, 5000)); // waiting for 5 seconds so that id does not get stored in the queue
+    // before the code gets deployed to R2
+    publisher.lPush("build-queue", id); // store the id in the queue shared by upload service and deploy service
+    publisher.hSet("status", id, "uploaded"); // store the status in redis regarding project being uploaded or deployed
     res.json({
         id: id,
     });
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,11 @@ import { getAllFiles } from "./file";
 import path from "path";
 import { uploadFile } from "./aws";
 import { createClient } from "redis";
-const publisher = createClient();
+
+const publisher = createClient(); // to store id in the queue
 publisher.connect();
 
-const subscriber = createClient();
+const subscriber = createClient(); // to store about the status being uploaded or deployed
 subscriber.connect();
 
 const app = express();
@@ -18,28 +19,28 @@ app.use(express.json());
 
 app.post("/deploy", async (req, res) => {
   const repoUrl = req.body.repoUrl;
-  const id = generate(); // asd12
-  await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
+  const id = generate(); // something like asd12
+  await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`)); // clones the repo in dist/output
 
   const files = getAllFiles(path.join(__dirname, `output/${id}`));
 
   files.forEach(async (file) => {
     const filename = path.relative(__dirname, file).replace(/\\/g, "/");
-    await uploadFile(filename, file);
+    await uploadFile(filename, file); // upload all the files to the R2 bucket
   });
 
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  publisher.lPush("build-queue", id);
-  // INSERT => SQL
-  // .create =>
-  publisher.hSet("status", id, "uploaded");
+  await new Promise((resolve) => setTimeout(resolve, 5000)); // waiting for 5 seconds so that id does not get stored in the queue
+  // before the code gets deployed to R2
+
+  publisher.lPush("build-queue", id);  // store the id in the queue shared by upload service and deploy service
+  publisher.hSet("status", id, "uploaded"); // store the status in redis regarding project being uploaded or deployed
 
   res.json({
     id: id,
   });
 });
 
-app.get("/status", async (req, res) => {
+app.get("/status", async (req, res) => {  // api to get the status of the project
   const id = req.query.id;
   const response = await subscriber.hGet("status", id as string);
   res.json({
